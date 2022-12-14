01f2ee9e155a7ad7a0d4b6a6c0e116906469f333
Gul <62958847+Gultoksoz@users.noreply.github.com>
2022-12-15 01:10:17 +0300

    veri çekme işlemleri

---
+++ lib/SharedPrefFayda.dart
@@ -0,0 +1,33 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefFayda{
+
+
+  static prefEmailKaydet(email) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('email',email);
+  }
+
+  static prefEmailSil(email) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('email');
+  }
+  static Future<String> getEmailPref () async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('email').toString();
+  }
+
+  static Color gecmisToplantilarColor = Color(0xFFFFFFFF);
+  static Color gelecekToplantilarColor = Color(0xFFFFFFFF);
+
+
+
+
+
+
+
+
+}
